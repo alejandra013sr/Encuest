51eab0b188d08142d75546f02f1f5a4693affd11
Lorenzo <lorppc1@gmail.com>
2021-07-06 21:46:22 -0400

    Se agrego la estructura y los estilos de la pagina principal

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <Tutorial/>
+  <div class="home__wrapper principal">
+    <header class="home__header">
+      <div class="home__logo">
+        <Logo />
+      </div>
+      <div class="home__title">
+        <span>Bienvenido a</span>
+        <div class="home__name">
+          <b>ENCUEST</b>
+          <span>APP</span>
+        </div>
+      </div>
+    </header>
+    <main class="home__main">
+      <a class="home__option">Login</a>
+      <a class="home__option">Register</a>
+    </main>
+  </div>
 </template>
 
 <script>
