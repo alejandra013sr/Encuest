c7113213a37bbdb6bb8241552ec6aac6336ac18a
alejandra013sr <alejandrasuescum40@gmail.com>
2021-08-05 02:55:03 -0400

    Subir imagenes actualizado
    Se le asigna la imagen subida al usuario que la subio

--- encuesta/imagenes/api.py
+++ encuesta/imagenes/api.py
@@ -3,9 +3,12 @@ from rest_framework.serializers import Serializer
 from imagenes.models import Imagen
 from imagenes.serializers import ImagenSerializer
 
+from profile_api.models import UserProfile
+
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 class ImagenAPI(APIView):
 
@@ -16,17 +19,32 @@ class ImagenAPI(APIView):
 
     def post(self,request):
         imagen_serializer = ImagenSerializer(data=request.data)
-        
+        user = UserProfile.objects.filter(email=request.data["email"]).first()
+
         if imagen_serializer.is_valid():
-            validated_data = imagen_serializer.validated_data
             
+
+            validated_data = imagen_serializer.validated_data
+    
             #Convertir y guardar el modelo
             imagen = Imagen(**validated_data)
+            imagen.user= user
             imagen.save()
 
             imagen_serializer=ImagenSerializer(imagen)
+      
+            
             return Response(imagen_serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response({'message': 'No se pudo guardar correctamente'},status=status.HTTP_400_BAD_REQUEST)
 
 
+    def get_object(self,pk):
+        image = get_object_or_404(Imagen,pk)
+        return image
+
+    def delete(self,request,pk):
+        image = self.get_object(pk)
+        image.image.delete(save=True)
+        image.delete()
+        return Response({"mensaje":"Imagen borrada"}, status=status.HTTP_200_OKs)
--- encuesta/imagenes/serializers.py
+++ encuesta/imagenes/serializers.py
@@ -5,22 +5,24 @@ from drf_extra_fields.fields import Base64ImageField
 from imagenes.models import Imagen
 from rest_framework import serializers
 
+from profile_api.models import UserProfile
+
 
 
 class ImagenSerializer(serializers.ModelSerializer):
     image = Base64ImageField(required=False)
     class Meta:
         model = Imagen
-        fields= ('title','description','image')
+        fields= ('title','description','image','user')
   
-
+    #No se esta llamando porque no se esta ejecutando un save() en el serializer que esta en api
     def create(self, validated_data):
-      
         ##Validated_data es un diccionario que contiene todos los fields enviados
         image = Imagen()
         image.title = validated_data.get("title")
    
         image.description = validated_data.get("description")
+        
         image.save()
 
         return image
